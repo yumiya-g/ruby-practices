e75a74fbdabd073369dddcdf272d221913aa0206
yumiya-g <52166120+yumiya-g@users.noreply.github.com>
2022-06-22 23:15:29 +0900

    Fix:カレンダープログラムを修正
    - 日付データの取得条件を見直し
    - 引数による条件分岐を削除
    - 色反転の条件を修正

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -8,45 +8,27 @@ get_month_year = ARGV.getopts('m:y:')
 # 今日の日付を取得
 today = Date.today
 
-# -m と -y に引数を指定
-if get_month_year["m"] && get_month_year["y"]
-  first_day_of_month = Date.new(get_month_year["y"].to_i, get_month_year["m"].to_i, 1)
-  last_day_of_month = Date.new(get_month_year["y"].to_i, get_month_year["m"].to_i, -1)
-  set_year = get_month_year["y"].to_i
-  set_month = get_month_year["m"].to_i
-# -m だけに引数を指定
-elsif get_month_year["m"]
-  first_day_of_month = Date.new(today.year, get_month_year["m"].to_i, 1)
-  last_day_of_month = Date.new(today.year, get_month_year["m"].to_i, -1)
-  set_year = today.year
-  set_month = get_month_year["m"].to_i
-# -y だけに引数を指定すると、処理を終了  
-elsif get_month_year["y"]
-  puts "コマンドライン引数に「#{get_month_year["y"]}」を設定しました"
-  return
-else
-# 引数がなければ、今月の開始日と終了日を取得
-  first_day_of_month = Date.new(today.year, today.mon, 1)
-  last_day_of_month = Date.new(today.year, today.mon, -1)
-  set_year = today.year
-  set_month = today.month
-end
+# 年・月を設定（引数がなければ、今日の年月）
+year = get_month_year["y"] || today.year
+month = get_month_year["m"] || today.month
+# 毎月の1日目と最終日を設定
+first_day_of_month = Date.new(year.to_i, month.to_i, 1)
+last_day_of_month = Date.new(year.to_i, month.to_i, -1)
 
 # カレンダー整形
 6.times{ print "\s" }
-puts "#{set_month}月\s#{set_year}年"
+puts "#{month}月\s#{year}年"
 puts "日 月 火 水 木 金 土"
 (first_day_of_month.wday * 3).times{ print "\s" } # 初日の曜日位置調整
 (first_day_of_month..last_day_of_month).each do |day|
   # 今日の日付をハイライト
-  if (set_month == today.month && set_year == today.year) && day.strftime(format = '%e') == today.day.to_s
+  if day == today
     print "\e[7m"
     print day.strftime(format = '%e')
     print "\e[0m"
   else
     print day.strftime(format = '%e')
   end
-  
   print "\s"
   puts ("\n") if day.saturday? # 土曜で改行を入れる
 end

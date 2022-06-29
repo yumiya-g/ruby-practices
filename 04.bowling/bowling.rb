25981e2bc4817e65dd6c986359fe0e07adb56950
yumiya-g <52166120+yumiya-g@users.noreply.github.com>
2022-06-29 22:04:55 +0900

    ボウリングのスコア計算プログラムを作成

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -6,7 +6,7 @@ scores = score.split(',') # カンマで引数を分割
 
 shots = [] # 各フレームの得点を格納する配列
 
-scores.each do |s| # ストライク（X）を[10, 0]変換して配列に格納
+scores.each do |s| # ストライク（X）を[10, 0]に変換して配列に格納
   if s == 'X'
     shots << 10
     shots << 0
@@ -15,18 +15,37 @@ scores.each do |s| # ストライク（X）を[10, 0]変換して配列に格納
   end
 end
 
-frames = [] # フレームごとに得点を分けて、2次元配列にする
+frames = [] # フレームごとに1投目と2投目を分けて、2次元配列にする
 shots.each_slice(2) do |shot|
   frames << shot
 end
 
-# 10フレームはストライク時に1投目の点数のみ取得して平坦化する
-frames_10th = frames[9..11]
-frames_10th.each do |f|
+# 10フレームは、ストライク時に1投目の点数を取得して2投目（0点）は削除
+# 最大3投分の配列が入った配列を平坦化
+frame_10th = frames[9..11]
+frame_10th.each do |f|
   f.pop if f.first == 10
 end
-p frames_10th.flatten!
+frame_10th.flatten!
 
-# 1~9フレームまでの点数を配列に格納して、10フレーム目の配列を末尾に追加
-p frames = frames[0..8]
-p frames << frames_10th
+# 1〜9フレームまでの点数を格納した配列に、10フレーム目の配列を追加
+frames = frames[0..8] << frame_10th
+
+points = 0
+
+frames.each.with_index(1) do |frame, i|
+  if frame.first == 10 # Strike
+    return p points += 10 + frame[1..2].sum if i == 10 # 第10フレームのストライク集計
+
+    points += if frame == [10, 0] && frames[i] == [10, 0] # ストライクが連続する場合
+                10 + frames[i].first + frames[i + 1].first
+              else
+                10 + frames[i][0..1].sum
+              end
+  elsif frame.sum == 10 # Spare
+    points += 10 + frames[i].first
+  else
+    points += frame.sum
+  end
+end
+p points

215c3c349a1682903dd0ec3f3bddcb61f0cab059
Yumiya <52166120+yumiya-g@users.noreply.github.com>
2022-07-13 23:06:54 +0900

    第2回レビューを参考にコードを修正

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -19,32 +19,39 @@ frames = shots.each_slice(2).to_a
 
 # 第10フレームは最大3投になるので、3投分の点数を平坦化して配列に格納
 # ex [10, 0, 10, 10] => [10, 10, 10]に変更する
-frame_10th = frames[9..11].flatten
-frame_10th.delete(0)
+frame_10th_processing = frames[9..11].flatten
+frame_10th = frame_10th_processing.reject(&:zero?)
 
 frame_1st_to_9th = frames[0..8]
-all_frames = frame_1st_to_9th << frame_10th
+p all_frames = [*frame_1st_to_9th << frame_10th]
 
-points = 0
+total_point = 0
 
-all_frames.each.with_index(1) do |frame, i|
-  if frame.first == 10 # Strike
-    points += frame.sum if i == 10
-    break if all_frames[i].nil?
+all_frames.each.with_index(1) do |frame, frame_nth|
+  strike = frame.first == 10
+  spare = !strike && frame.sum == 10
+  next_frame = all_frames[frame_nth]
+  after_next_frame = all_frames[frame_nth + 1]
 
-    points += if all_frames[i].first == 10
-                if i < 9
-                  10 + all_frames[i].first + all_frames[i + 1].first
-                elsif i == 9
-                  frame.sum + all_frames[i][0..1].sum
-                end
-              else
-                10 + all_frames[i][0..1].sum
-              end
-  elsif frame.sum == 10 # Spare
-    points += 10 + all_frames[i].first
+  if strike
+    if frame_nth == 10
+      total_point += frame.sum
+    else
+      next_frame_strike = next_frame[0] == 10
+      total_point += if next_frame_strike
+                       if frame_nth < 9
+                         10 + next_frame[0] + after_next_frame[0]
+                       elsif frame_nth == 9
+                         10 + next_frame[0..1].sum
+                       end
+                     else
+                       10 + next_frame[0..1].sum
+                     end
+    end
+  elsif spare
+    total_point += 10 + next_frame[0]
   else
-    points += frame.sum
+    total_point += frame.sum
   end
 end
-puts points
+puts total_point

9579751813d2facdfee32d4cec448ed7c1fe32de
yumiya-g <52166120+yumiya-g@users.noreply.github.com>
2022-07-16 22:08:44 +0900

    第3回レビューを参考にコードを修正
    - 変数「all_frames」に非破壊的に処理して代入
    - 全フレームの合計処理を処理を修正

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -19,24 +19,23 @@ frames = shots.each_slice(2).to_a
 
 # 第10フレームは最大3投になるので、3投分の点数を平坦化して配列に格納
 # ex [10, 0, 10, 10] => [10, 10, 10]に変更する
-frame_10th_processing = frames[9..11].flatten
-frame_10th = frame_10th_processing.reject(&:zero?)
+shots_for_10th = frames[9..11].flatten
+frame_10th = shots_for_10th.reject(&:zero?)
 
 frame_1st_to_9th = frames[0..8]
-all_frames = [*frame_1st_to_9th << frame_10th]
+all_frames = [*frame_1st_to_9th, frame_10th]
 
-total_point = all_frames.each.with_index(1).sum do |frame, frame_nth|
+total_point = all_frames.size.times.sum do |nth|
+  frame, next_frame, after_next_frame = all_frames[nth, 3]
   strike = frame.first == 10
   spare = !strike && frame.sum == 10
-  next_frame = all_frames[frame_nth]
-  after_next_frame = all_frames[frame_nth + 1]
 
   if strike
-    if frame_nth == 10
+    if nth == 9
       frame.sum
     else
       next_frame_strike = next_frame[0] == 10
-      if next_frame_strike && frame_nth < 9
+      if next_frame_strike && nth < 8
         10 + next_frame[0] + after_next_frame[0]
       else
         10 + next_frame[0..1].sum

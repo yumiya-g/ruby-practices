817c5ed59e6d34ee5f6ed245568ebbf969f93bc7
yumiya-g <52166120+yumiya-g@users.noreply.github.com>
2022-09-13 12:37:01 +0900

    Fix: 第2回レビューを参考にコードを修正

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -3,18 +3,28 @@
 
 MAX_COLUMNS = 3
 
-all_files = Dir.glob('*', base: 'test_files')
+def main
+  all_files = if ARGV.empty?
+                Dir.glob('*')
+              else
+                other_directory_files = []
+                ARGV.each do |str|
+                  other_directory_files = Dir.glob("#{str}/*")
+                end
+                other_directory_files
+              end
 
-def main(all_files)
-  counted_rows = calc_rows(all_files)
+  num_of_rows = calc_rows(all_files)
 
-  first_row = (0...counted_rows).map { |i| all_files[i] }
+  first_row = all_files[0...num_of_rows]
 
-  after_next_rows = create_after_second_rows(first_row, counted_rows, all_files)
+  after_next_rows = create_after_next_rows(first_row, num_of_rows, all_files)
 
-  file_names_list = swap_row_to_column(first_row, after_next_rows)
+  file_names = swap_row_to_column(first_row, after_next_rows)
 
-  output_file_name(file_names_list, all_files)
+  max_filename_length = count_max_filename(file_names)
+
+  output_file_name(file_names, max_filename_length)
 end
 
 def calc_rows(all_files)
@@ -22,11 +32,11 @@ def calc_rows(all_files)
   (num_of_files.to_f / MAX_COLUMNS).ceil
 end
 
-def create_after_second_rows(first_row, counted_rows, all_files)
+def create_after_next_rows(first_row, num_of_rows, all_files)
   after_second_row = []
   remaining_files = all_files - first_row
 
-  remaining_files.each_slice(counted_rows) { |array| after_second_row << array }
+  remaining_files.each_slice(num_of_rows) { |array| after_second_row << array }
   after_second_row
 end
 
@@ -34,14 +44,20 @@ def swap_row_to_column(first_row, after_next_rows)
   first_row.zip(*after_next_rows)
 end
 
-def output_file_name(file_names_list, all_files)
-  max_filename_length = all_files.map(&:length).max
-  file_names_list.each do |file_names|
-    file_names.each do |file_name|
-      print "#{file_name.ljust(max_filename_length)}\s" unless file_name.nil?
+def count_max_filename(file_names)
+  formatted_file_names = file_names.flatten.compact.map do |name_str|
+    name_str.gsub(%r{.*/}, '')
+  end
+  formatted_file_names.map(&:size).max
+end
+
+def output_file_name(file_names, max_filename_length)
+  file_names.each do |file_name|
+    file_name.each do |name|
+      print "#{name.gsub(%r{.*/}, '').ljust(max_filename_length)}\s" unless name.nil?
     end
-    print "\n"
+    puts
   end
 end
 
-main(all_files)
+main

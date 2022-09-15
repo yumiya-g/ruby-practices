7a6d95d062e6a0bee4ac54cf512e3e5cff9b6ae8
yumiya-g <52166120+yumiya-g@users.noreply.github.com>
2022-09-16 08:38:51 +0900

    Fix: 修了後のコメント対応
    - ファイルの取得処理をfetch_file_namesメソッドに分割
    - ファイルパスの整形をformatted_file_namesで処理

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -4,15 +4,7 @@
 MAX_COLUMNS = 3
 
 def main
-  all_files = if ARGV.empty?
-                Dir.glob('*')
-              else
-                other_directory_files = []
-                ARGV.each do |str|
-                  other_directory_files = Dir.glob("#{str}/*")
-                end
-                other_directory_files
-              end
+  all_files = formatted_file_names(fetch_file_names)
 
   num_of_rows = calc_rows(all_files)
 
@@ -22,9 +14,27 @@ def main
 
   file_names = swap_row_to_column(first_row, after_next_rows)
 
-  max_filename_length = count_max_filename(file_names)
+  max_filename_length = count_max_filename(all_files)
 
   output_file_name(file_names, max_filename_length)
+end
+
+def fetch_file_names
+  if ARGV.empty?
+    Dir.glob('*')
+  else
+    other_directory_files = []
+    ARGV.each do |str|
+      other_directory_files = Dir.glob("#{str}/*")
+    end
+    other_directory_files
+  end
+end
+
+def formatted_file_names(fetch_file_names)
+  fetch_file_names.map do |file_name_str|
+    file_name_str.gsub(%r{.*/}, '')
+  end
 end
 
 def calc_rows(all_files)
@@ -44,17 +54,14 @@ def swap_row_to_column(first_row, after_next_rows)
   first_row.zip(*after_next_rows)
 end
 
-def count_max_filename(file_names)
-  formatted_file_names = file_names.flatten.compact.map do |name_str|
-    name_str.gsub(%r{.*/}, '')
-  end
-  formatted_file_names.map(&:size).max
+def count_max_filename(all_files)
+  all_files.map(&:size).max
 end
 
 def output_file_name(file_names, max_filename_length)
   file_names.each do |file_name|
     file_name.each do |name|
-      print "#{name.gsub(%r{.*/}, '').ljust(max_filename_length)}\s" unless name.nil?
+      print "#{name.ljust(max_filename_length)}\s" unless name.nil?
     end
     puts
   end

78aefcc85ba8173c2218b976e4b5c72518df1fd3
yumiya-g <52166120+yumiya-g@users.noreply.github.com>
2023-04-03 23:33:55 +0900

    Fix: コマンドラインに複数ファイルを渡した時の、ファイル情報の合計処理を修正

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -4,7 +4,6 @@
 require 'optparse'
 require 'debug'
 
-OUTPUT_ORDER = %w[l w c].freeze
 OPTION_KEY_MAP = [%i[l rows], %i[w words], %i[c bytesize]].freeze
 
 NUMBER_OF_LINES = 8
@@ -62,6 +61,7 @@ def output_argv(parsed_argv)
 
   if parsed_argv[:options].empty?
     inputs.map do |input|
+      # TODO: DRYにする
       output_value(input[:rows])
       output_value(input[:words])
       output_value(input[:bytesize])
@@ -70,13 +70,14 @@ def output_argv(parsed_argv)
   else
     output_options_available(inputs, parsed_argv)
   end
-  sum_values(inputs, sorted_options) if inputs.count >= 2
+
+  sum_values(inputs, parsed_argv) if inputs.count >= 2
 end
 
 def output_options_available(inputs, parsed_argv)
   inputs.map do |input|
     OPTION_KEY_MAP.each do |option, key|
-      output_value(input[key]) if parsed_argv[:options].nil? || parsed_argv[:options].include?(option)
+      output_value(input[key]) if parsed_argv[:options].include?(option)
     end
     puts "\s#{input[:filename]}"
   end
@@ -95,28 +96,15 @@ def parse_file_info(parsed_argv, *stdin)
 end
 
 def build_file_info(file_info, *name)
-  bytesize = file_info.bytesize
   formatted_inputs = file_info.split("\n").map { |s| s.split("\s") }
   rows = formatted_inputs.count
   words = formatted_inputs.map(&:count).sum
+  bytesize = file_info.bytesize
 
   { rows: rows, words: words, bytesize: bytesize, filename: name.first }
 end
 
-def sort_values_by_options(parsed_argv)
-  parsed_argv[:options].sort_by { |opt| OUTPUT_ORDER.index(opt.to_s) }
-end
-
-def output_option_values(inputs, options)
-  inputs.map do |input|
-    options.map do |option|
-      output_by_sorting_values(input, option)
-    end
-    puts "\s#{input[:filename]}"
-  end
-end
-
-def sum_values(inputs, sorted_options)
+def sum_values(inputs, parsed_argv)
   file_values = inputs.map do |input|
     input.except(:filename)
   end
@@ -125,26 +113,14 @@ def sum_values(inputs, sorted_options)
     current_hash + next_hash
   end
 
-  output_sum_values(total_values, sorted_options)
+  output_sum_values(total_values, parsed_argv)
 end
 
-def output_sum_values(total_values, sorted_options)
-  if sorted_options.nil?
-    total_values.values.map { |value| output_value(value) }
-  else
-    sorted_options.map do |option|
-      output_by_sorting_values(total_values, option)
-    end
+def output_sum_values(total_values, parsed_argv)
+  OPTION_KEY_MAP.each do |option, key|
+    output_value(total_values[key]) if parsed_argv[:options].empty? || parsed_argv[:options].include?(option)
   end
   puts "\stotal"
-end
-
-def output_by_sorting_values(input, option)
-  case option
-  when :l then output_value(input[:rows])
-  when :w then output_value(input[:words])
-  when :c then output_value(input[:bytesize])
-  end
 end
 
 def output_value(int)

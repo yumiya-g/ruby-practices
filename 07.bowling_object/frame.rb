441ccedeadf20ba260986a24e7231e8a964a3d30
yumiya-g <52166120+yumiya-g@users.noreply.github.com>
2024-06-17 08:39:04 +0900

    Fix: frameクラスおよびテストコードを修正しました

--- 07.bowling_object/frame.rb
+++ 07.bowling_object/frame.rb
@@ -1,22 +1,23 @@
 # frozen_string_literal: true
 
 require_relative 'shot'
-require 'debug'
 
 class Frame
-  # 各ショットをインスタンス変数に格納
   def initialize(first_mark, second_mark = nil, third_mark = nil)
     @first_shot = Shot.new(first_mark).score
     @second_shot = Shot.new(second_mark).score
     @third_shot = Shot.new(third_mark).score
   end
 
-  # フレームごとのスコアを生成する
   def create_scores_per_frame
     shots = []
     shots << @first_shot
-    shots << @second_shot unless @second_shot.nil?
-    shots << @third_shot unless @second_shot.nil?
+    shots << @second_shot if shot_exist?(@second_shot)
+    shots << @third_shot if shot_exist?(@second_shot)
     shots.compact
+  end
+
+  def shot_exist?(shot)
+    !shot.nil?
   end
 end

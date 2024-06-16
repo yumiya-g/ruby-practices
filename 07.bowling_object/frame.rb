7f951e6e4f87ef0c4cae5ef742f2efd9c9730d34
yumiya-g <52166120+yumiya-g@users.noreply.github.com>
2024-06-16 10:16:10 +0900

    Fix: Frameクラスの処理とテストケースを修正しました

--- 07.bowling_object/frame.rb
+++ 07.bowling_object/frame.rb
@@ -1,23 +1,22 @@
 # frozen_string_literal: true
 
-require_relative './shot.rb'
+require_relative 'shot'
+require 'debug'
 
 class Frame
-  attr_reader :first_shot, :second_shot, :third_shot
-
-  def initialize(first_mark, second_mark, third_mark = nil)
-    @first_shot = Shot.new(first_mark)
-    @second_shot = Shot.new(second_mark)
-    @third_shot = Shot.new(third_mark)
+  # 各ショットをインスタンス変数に格納
+  def initialize(first_mark, second_mark = nil, third_mark = nil)
+    @first_shot = Shot.new(first_mark).score
+    @second_shot = Shot.new(second_mark).score
+    @third_shot = Shot.new(third_mark).score
   end
 
-
-
-  # 1フレームのスコア合計
-  def score
-    shots = [first_shot.score, second_shot.score, third_shot.score]
-    shots.sum
+  # フレームごとのスコアを生成する
+  def create_scores_per_frame
+    shots = []
+    shots << @first_shot
+    shots << @second_shot unless @second_shot.nil?
+    shots << @third_shot unless @second_shot.nil?
+    shots.compact
   end
 end
-
-

ecff1c36b2f7195d8dfd98c74586a6e26cd2bd4c
yumiya-g <52166120+yumiya-g@users.noreply.github.com>
2024-08-08 22:11:11 +0900

    Fix: `Shot`クラスの条件分岐処理とメソッド名を見直しました
    - `nil`判定を削除

--- 07.bowling_object/test/shot_test.rb
+++ 07.bowling_object/test/shot_test.rb
@@ -6,19 +6,14 @@ require_relative '../shot'
 class ShotTest < Minitest::Test
   def test_score_x
     shot = Shot.new('X')
-    assert_equal 10, shot.score
+    assert_equal 10, shot.mark_to_score
   end
 
   def test_score_one_to_nine
     scores = [*0..9].map(&:to_s)
     scores.each do |score|
       shot = Shot.new(score)
-      assert_equal score.to_i, shot.score
+      assert_equal score.to_i, shot.mark_to_score
     end
-  end
-
-  def test_score_nil
-    shot = Shot.new(nil)
-    assert_nil(shot.score)
   end
 end

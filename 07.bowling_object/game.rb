eb8fb00109440df81546320bc3ecb03fd6efa0d3
yumiya-g <52166120+yumiya-g@users.noreply.github.com>
2024-08-08 22:13:30 +0900

    Fix: `Game`クラスが持つ責務を`Frame`クラスに移しました
    - shebangを削除
    - 各フレームの処理を`Frame`へ移譲

--- 07.bowling_object/game.rb
+++ 07.bowling_object/game.rb
@@ -1,90 +1,13 @@
-#!/usr/bin/env ruby
 # frozen_string_literal: true
-
-require 'debug'
 
 require_relative 'frame'
 
 class Game
-  def initialize(marks)
-    @all_shots = marks
+  def initialize(scores)
+    @scores = scores
   end
 
-  def calc_scores
-    all_frames = parse_all_frames
-    scores = score_per_frames(all_frames)
-    sum(scores)
-  end
-
-  private
-
-  def parse_all_frames
-    all_frames = []
-    frame = []
-    @all_shots.each do |shot|
-      frame << shot
-      if all_frames.size < 10
-        if frame.size >= 2 || shot == 'X'
-          all_frames << frame.dup
-          frame.clear
-        end
-      else
-        all_frames.last << shot
-      end
-    end
-    all_frames
-  end
-
-  def score_per_frames(frames)
-    frames.map do |frame|
-      frame = Frame.new(*frame)
-      frame.create_scores
-    end
-  end
-
-  def sum(scores)
-    point_sum = 0
-    scores.size.times do |frame_number|
-      frame, next_frame, after_next_frame = scores[frame_number, 3]
-      next_frame ||= []
-      after_next_frame ||= []
-
-      point = 0
-      if one_to_nine_frame?(frame_number)
-        point = total_score_to_point(point, frame, next_frame, after_next_frame)
-      else
-        point += frame.sum
-      end
-      point_sum += point
-    end
-    point_sum
-  end
-
-  def total_score_to_point(point, frame, next_frame, after_next_frame)
-    if strike?(frame)
-      point += frame.sum + next_frame[0..1].sum
-      point += after_next_frame.first if next_strike?(next_frame)
-    elsif spare?(frame)
-      point += frame.sum + next_frame.first
-    else
-      point += frame[0..1].sum
-    end
-    point
-  end
-
-  def strike?(frame)
-    frame.first == 10
-  end
-
-  def next_strike?(frame)
-    frame.count == 1
-  end
-
-  def spare?(frame)
-    frame[0..1].sum == 10
-  end
-
-  def one_to_nine_frame?(frame_number)
-    frame_number < 9
+  def sum
+    @scores.sum
   end
 end
--- 07.bowling_object/test/game_test.rb
+++ 07.bowling_object/test/game_test.rb
@@ -4,63 +4,51 @@ require 'minitest/autorun'
 require_relative '../game'
 
 class GameTest < Minitest::Test
-  def mark_split(marks)
-    marks.split(',').map { |s| s }
-  end
-
   def test_calc_scores_mark1
-    marks = '6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,6,4,5'
-    all_shots = mark_split(marks)
-    game = Game.new(all_shots)
-    assert_equal 139, game.calc_scores
+    marks = '6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,6,4,5'.split(',').map { |s| s }
+    scores_per_frames = Frame.new(marks).calc_scores
+    assert_equal 139, Game.new(scores_per_frames).sum
   end
 
   def test_calc_scores_mark2
-    marks = '6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,X,X'
-    all_shots = mark_split(marks)
-    game = Game.new(all_shots)
-    assert_equal 164, game.calc_scores
+    marks = '6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,X,X'.split(',').map { |s| s }
+    scores_per_frames = Frame.new(marks).calc_scores
+    assert_equal 164, Game.new(scores_per_frames).sum
   end
 
   def test_calc_scores_mark3
-    marks = '0,10,1,5,0,0,0,0,X,X,X,5,1,8,1,0,4'
-    all_shots = mark_split(marks)
-    game = Game.new(all_shots)
-    assert_equal 107, game.calc_scores
+    marks = '0,10,1,5,0,0,0,0,X,X,X,5,1,8,1,0,4'.split(',').map { |s| s }
+    scores_per_frames = Frame.new(marks).calc_scores
+    assert_equal 107, Game.new(scores_per_frames).sum
   end
 
   def test_calc_scores_mark4
-    marks = '6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,0,0'
-    all_shots = mark_split(marks)
-    game = Game.new(all_shots)
-    assert_equal 134, game.calc_scores
+    marks = '6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,0,0'.split(',').map { |s| s }
+    scores_per_frames = Frame.new(marks).calc_scores
+    assert_equal 134, Game.new(scores_per_frames).sum
   end
 
   def test_calc_scores_mark5
-    marks = '6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,1,8'
-    all_shots = mark_split(marks)
-    game = Game.new(all_shots)
-    assert_equal 144, game.calc_scores
+    marks = '6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,1,8'.split(',').map { |s| s }
+    scores_per_frames = Frame.new(marks).calc_scores
+    assert_equal 144, Game.new(scores_per_frames).sum
   end
 
   def test_calc_scores_mark6
-    marks = 'X,X,X,X,X,X,X,X,X,X,X,X'
-    all_shots = mark_split(marks)
-    game = Game.new(all_shots)
-    assert_equal 300, game.calc_scores
+    marks = 'X,X,X,X,X,X,X,X,X,X,X,X'.split(',').map { |s| s }
+    scores_per_frames = Frame.new(marks).calc_scores
+    assert_equal 300, Game.new(scores_per_frames).sum
   end
 
   def test_calc_scores_mark7
-    marks = 'X,X,X,X,X,X,X,X,X,X,X,2'
-    all_shots = mark_split(marks)
-    game = Game.new(all_shots)
-    assert_equal 292, game.calc_scores
+    marks = 'X,X,X,X,X,X,X,X,X,X,X,2'.split(',').map { |s| s }
+    scores_per_frames = Frame.new(marks).calc_scores
+    assert_equal 292, Game.new(scores_per_frames).sum
   end
 
   def test_calc_scores_mark8
-    marks = 'X,0,0,X,0,0,X,0,0,X,0,0,X,0,0'
-    all_shots = mark_split(marks)
-    game = Game.new(all_shots)
-    assert_equal 50, game.calc_scores
+    marks = 'X,0,0,X,0,0,X,0,0,X,0,0,X,0,0'.split(',').map { |s| s }
+    scores_per_frames = Frame.new(marks).calc_scores
+    assert_equal 50, Game.new(scores_per_frames).sum
   end
 end

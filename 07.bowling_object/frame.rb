f7d2130fab0870095aee19f2fcc2e47593d684d6
yumiya-g <52166120+yumiya-g@users.noreply.github.com>
2024-08-08 22:17:17 +0900

    Fix: `Frame`クラスでARGVに渡された引数を処理するよう変更しました
    - `private`を設定
    - 変数名を見直し

--- 07.bowling_object/frame.rb
+++ 07.bowling_object/frame.rb
@@ -2,24 +2,84 @@
 
 require_relative 'shot'
 
-require "debug"
-
 class Frame
-  def initialize(first_mark, second_mark = nil, third_mark = nil)
-    @first_shot = Shot.new(first_mark).score
-    @second_shot = Shot.new(second_mark).score
-    @third_shot = Shot.new(third_mark).score
+  def initialize(all_shots)
+    @all_shots = all_shots
   end
 
-  def create_scores
-    shots = []
-    shots << @first_shot
-    shots << @second_shot if shot_exist?(@second_shot)
-    shots << @third_shot if shot_exist?(@third_shot)
-    shots.compact
+  def calc_scores
+    all_shots = parse_all_shots
+    convert_shots_to_scores(all_shots)
+  end
+
+  private
+
+  def parse_all_shots
+    all_frames = []
+    frame = []
+    @all_shots.each do |shot|
+      shot = Shot.new(shot).mark_to_score
+
+      frame << shot
+      if all_frames.size < 10
+        if frame.size >= 2 || shot == 10
+          all_frames << frame.dup
+          frame.clear
+        end
+      else
+        all_frames.last << shot
+      end
+    end
+    all_frames
+  end
+
+  def convert_shots_to_scores(all_shots)
+    scores = []
+    all_shots.size.times do |shots|
+      frame, next_frame, after_next_frame = all_shots[shots, 3]
+      next_frame ||= []
+      after_next_frame ||= []
+
+      scores << if within_number_one_to_nine?(shots)
+                  total_score_to_point(frame, next_frame, after_next_frame)
+                else
+                  frame.sum
+                end
+    end
+    scores
+  end
+
+  def total_score_to_point(frame, next_frame, after_next_frame)
+    point = 0
+
+    if strike?(frame)
+      point += frame.sum + next_frame[0..1].sum
+      point += after_next_frame.first if next_strike?(next_frame)
+    elsif spare?(frame)
+      point += frame.sum + next_frame.first
+    else
+      point += frame[0..1].sum
+    end
+    point
   end
 
   def shot_exist?(shot)
     !shot.nil?
+  end
+
+  def strike?(frame)
+    frame.first == 10
+  end
+
+  def next_strike?(frame)
+    frame.count == 1
+  end
+
+  def spare?(frame)
+    frame[0..1].sum == 10
+  end
+
+  def within_number_one_to_nine?(frame_number)
+    frame_number < 9
   end
 end
--- 07.bowling_object/test/frame_test.rb
+++ 07.bowling_object/test/frame_test.rb
@@ -4,28 +4,4 @@ require 'minitest/autorun'
 require_relative '../frame'
 
 class FrameTest < Minitest::Test
-  def test_scores_strike
-    frame = Frame.new('X')
-    assert_equal frame.create_scores_per_frame, [10]
-  end
-
-  def test_scores_spare
-    frame = Frame.new('1', '9')
-    assert_equal frame.create_scores_per_frame, [1, 9]
-  end
-
-  def test_scores_less_than_ten
-    frame = Frame.new('1', '0')
-    assert_equal frame.create_scores_per_frame, [1, 0]
-  end
-
-  def test_ten_frame_strike
-    frame = Frame.new('X', '0', 'X')
-    assert_equal frame.create_scores_per_frame, [10, 0, 10]
-  end
-
-  def test_ten_frame_spare
-    frame = Frame.new('4', '6', 'X')
-    assert_equal frame.create_scores_per_frame, [4, 6, 10]
-  end
 end

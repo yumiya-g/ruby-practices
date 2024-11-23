be381daa027c620fff605de1152e8c0826b9bbfa
yumiya-g <52166120+yumiya-g@users.noreply.github.com>
2024-11-23 16:34:01 +0900

    Fix: Rowクラスを修正しました
    - `l`と`r`オプションを持っている場合の条件処理を追加
    - `l`の有無で、ファイル名またはファイル情報を出力する処理を切り替える
    - 不要なコードやコメント等を削除

--- 08.ls_object/row.rb
+++ 08.ls_object/row.rb
@@ -1,43 +1,59 @@
 # frozen_string_literal: true
 
-require 'debug'
-
 MAX_COLUMNS = 3
 
 class Row
   attr_reader :file_stats
 
   def initialize(file_stats, options = [])
-    @file_stats = file_stats.compact # 隠しファイルがnilになった場合、除外している
     @options = options
+    @file_stats = @options.include?(:r) ? file_stats.reverse.compact : file_stats.compact
+    options.include?(:l) ? display_stats : display_name
   end
 
+  private
+
   def display_stats
-    nlink_max_column = file_stats.max_by{|fs| fs[:nlink]}[:nlink].to_s.size
-    filesize_max_column = file_stats.max_by{|fs| fs[:size]}[:size].to_s.size
-    
-    puts "total #{file_stats.sum {|fs| fs[:blocks]}}"
+    puts "total #{file_stats.sum { |fs| fs[:blocks] }}"
+    puts output_columns
+  end
+
+  def output_columns
+    digits = generate_file_stats_digits
     file_stats.map do |fs|
-      puts "#{fs[:permission]} #{fs[:nlink].to_s.rjust(nlink_max_column)} #{fs[:owner]}  #{fs[:group]}  #{fs[:size].to_s.rjust(filesize_max_column)} #{fs[:date][:month]} #{fs[:date][:day]} #{fs[:date][:hour]}:#{fs[:date][:min]} #{fs[:name]}"
+      [
+        fs[:permission],
+        fs[:nlink].to_s.rjust(digits[:nlink]),
+        fs[:owner].to_s.rjust(digits[:owner]),
+        fs[:group].to_s.rjust(digits[:group] + 1),
+        fs[:size].to_s.rjust(digits[:size] + 1),
+        "#{fs[:date][:month]} #{fs[:date][:day]}",
+        "#{fs[:date][:hour]}:#{fs[:date][:min]} #{fs[:name]}"
+      ].join("\s")
     end
   end
 
   def display_name
-    rows = (file_stats.size.to_f / MAX_COLUMNS).ceil # 商切り上げで列数を出す
-    sliced_file_name = file_stats.each_slice(rows).to_a # ファイル名を3つずつ配列にする
-    adjusted_names = adjust_padding(sliced_file_name) # ファイル名の文字列を調整する
-
+    rows = (file_stats.size.to_f / MAX_COLUMNS).ceil
+    adjusted_names = adjust_name_padding.each_slice(rows).to_a
     adjusted_names.first.zip(*adjusted_names[1..]).map do |name|
       puts name.join("\s")
     end
   end
 
-  def adjust_padding(sliced_file_name) # TODO:ここでファイル名だけでなくstatsの各情報を調整したい
-    max_number_chars = file_stats.max_by(&:bytesize).bytesize
-    sliced_file_name.map do |names|
-      names.map do |name|
-        name.ljust(max_number_chars)
-      end
+  def generate_file_stats_digits
+    {
+      nlink: file_stats.max_by { |fs| fs[:nlink] }[:nlink].to_s.size,
+      size: file_stats.max_by { |fs| fs[:size] }[:size].to_s.size,
+      owner: file_stats.max_by { |fs| fs[:owner] }[:owner].to_s.size,
+      group: file_stats.max_by { |fs| fs[:group] }[:group].to_s.size
+    }
+  end
+
+  def adjust_name_padding
+    max_number_chars = file_stats.max_by { |fs| fs[:name].size }[:name].size
+    file_stats.map do |fs|
+      fs[:name].to_s.ljust(max_number_chars)
     end
   end
 end

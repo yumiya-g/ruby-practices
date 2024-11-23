98d78dbcba7d757c6af9175c2dcc0a54af8350df
yumiya-g <52166120+yumiya-g@users.noreply.github.com>
2024-11-23 16:29:24 +0900

    Fix: Commandクラスを修正しました
    - `l`オプションを持っている場合の条件分岐処理を削除
    - `a`オプションを持っている場合の条件分岐処理を追加
    - 不要なコードやコメントを削除

--- 08.ls_object/command.rb
+++ 08.ls_object/command.rb
@@ -1,12 +1,9 @@
 # frozen_string_literal: true
 
 require 'optparse'
-require 'debug'
-
 require_relative 'file'
 require_relative 'row'
 
-# コマンドを受け取って、オプションとディレクトリに分ける処理を行なう
 class Command
   attr_reader :argv, :options, :directory_name, :file_stats
 
@@ -18,17 +15,12 @@ class Command
   end
 
   def display_files
-    if options.include?(:l)
-      Row.new(file_stats, options).display_stats
-    else
-      Row.new(file_stats, options).display_name
-    end
+    Row.new(file_stats, options)
   end
 
   private
 
   def parse_command
-    # OptionParserでコマンドライン引数を[:a,:r.:l]に分ける
     opt = OptionParser.new
     opt.on('-a') { |v| v }
     opt.on('-r') { |v| v }
@@ -38,12 +30,12 @@ class Command
     { directory_name: directory_name, options: options }
   end
 
-  # ディレクトリから各ファイル情報を取得する
   def fetch_files
     dir_name = directory_name.empty? ? '*' : "#{directory_name.first}/*"
-    files = Dir.glob(dir_name, ::File::FNM_DOTMATCH)
+    files = options.include?(:a) ? Dir.glob(dir_name, ::File::FNM_DOTMATCH) : Dir.glob(dir_name)
+
     files.map do |file|
-      Ls::File.new(file, options).generate_stats
+      Ls::File.new(file, options).generate_file_stats
     end
   end
 end

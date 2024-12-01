0a1568c3cb5b45371ba34ad2867ec438d2e8bea9
yumiya-g <52166120+yumiya-g@users.noreply.github.com>
2024-12-02 08:15:18 +0900

    Fix: インスタンス変数の持ち方を変更して、パプリックメソッドでプリミティブな値に変換しました

--- 08.ls_object/file.rb
+++ 08.ls_object/file.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require 'etc'
-require_relative 'row'
 
 FILE_TYPE = {
   'file': '-',
@@ -22,45 +21,37 @@ FILE_PERMISSION = {
 
 module Ls
   class File
-    attr_reader :file_stats, :date, :permission
+    attr_reader :file_stats
 
-    def initialize(file = nil, options = [])
-      @options = options
-      @file_stats = ::File::Stat.new(file)
-      @blocks = file_stats.blocks
-      @type = file_stats.ftype
-      @permission = convert_characters(file_stats.mode)
-      @nlink = file_stats.nlink
-      @owner = Etc.getpwuid(file_stats.uid).name
-      @group = Etc.getgrgid(file_stats.gid).name
-      @size = file_stats.size
-      @date = generate_date_object
-      @name = ::File.basename(file)
-    end
-
-    def generate_file_stats
-      {
-        blocks: @blocks,
-        permission: @permission,
-        nlink: @nlink,
-        owner: @owner,
-        group: @group,
-        size: @size,
-        date: @date,
-        name: @name
-      }
+    def initialize(file_name = nil)
+      @file_name = file_name
+      @file_stats = generate_file_stats(::File::Stat.new(file_name))
     end
 
     private
 
-    def convert_characters(permission)
-      chars = permission.to_s(8)[-3..].chars.map do |p|
-        FILE_PERMISSION[p.to_sym]
-      end
-      FILE_TYPE[@type.to_sym] + chars.join
+    def generate_file_stats(file_stats)
+      {
+        blocks: file_stats.blocks,
+        type: file_stats.ftype,
+        permission: convert_characters(file_stats),
+        nlink: file_stats.nlink,
+        owner: Etc.getpwuid(file_stats.uid).name,
+        group: Etc.getgrgid(file_stats.gid).name,
+        size: file_stats.size,
+        date: generate_date_object(file_stats),
+        name: ::File.basename(@file_name)
+      }
     end
 
-    def generate_date_object
+    def convert_characters(file_stats)
+      chars = file_stats.mode.to_s(8)[-3..].chars.map do |fs|
+        FILE_PERMISSION[fs.to_sym]
+      end
+      FILE_TYPE[file_stats.ftype.to_sym] + chars.join
+    end
+
+    def generate_date_object(file_stats)
       {
         year: file_stats.mtime.year.to_s,
         month: file_stats.mtime.month.to_s.rjust(2, '0'),
